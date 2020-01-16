1d860d846c661cd1c321d1847389c038613f2292
Noah <cxhmel@outlook.com>
2020-01-16 13:57:14 +0800

    Merge branch 'master' of https://github.com/NoahELE/SpaceFight

--- alien.py
+++ alien.py
@@ -8,25 +8,5 @@ class Alien(Sprite):
         super().__init__()
         self.screen = screen
         self.sf_settings = sf_settings
-
         self.image = pygame.image.load('images/alien.png')
-        self.rect = self.image.get_rect()
 
-        self.rect.x = self.rect.width
-        self.rect.y = self.rect.height
-
-        self.x = float(self.rect.x)
-
-    def blitme(self):
-        self.screen.blit(self.image, self.rect)
-
-    def update(self):
-        self.x += (self.sf_settings.alien_speed * self.sf_settings.alien_direction)
-        self.rect.x = self.x
-
-    def check_edges(self):
-        screen_rect = self.screen.get_rect()
-        if self.rect.right >= screen_rect.right:
-            return True
-        elif self.rect.left <= 0:
-            return True
